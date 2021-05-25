59eb78c1c0f58c12829fb0bd8da7ec944ea68899
Min Khant Kyaw <kophyoe276student@gmail.com>
2021-05-25 10:32:42 +0630

    Updating files

--- js/jquery.js
+++ js/jquery.js
@@ -1,26 +1,38 @@
 $(document).ready(function(){
 
-   $('#navbarDropdown').on('click', () => {
-      $('#dropdown').toggle();
+
+   /* Preloader */
+	$(window).on('load', function() {
+		var preloaderFadeOutTime = 3;
+		function hidePreloader() {
+			var preloader = $('.spinner-wrapper');
+			setTimeout(function() {
+				preloader.fadeOut(preloaderFadeOutTime);
+			}, 3);
+		}
+		hidePreloader();
+	});  
+   
+   $('#navbarDropdown').click (function() {
+      $('#dropdown').slideToggle('slow');
    });
 
    $('#navbarDropdown1').on('click', () => {
-   	 $('#dropdown1').toggle();
-       $('#dropdown2').fadeOut();
+   	 $('#dropdown1').slideToggle('slow');
+       $('#dropdown2').fadeOut(500);
    });
 
    $('#navbarDropdown2').on('click', () => {
-   	 $('#dropdown2').toggle();
-       $('#dropdown1').fadeOut();
+   	 $('#dropdown2').slideToggle('slow');
+       $('#dropdown1').fadeOut(500);
    });
-   
 
-   // $('#navbarDropdown2').on('click', () => {
-   // 	  $('#dropdown1').fadeOut('fast');
-   // });
-    
-   // $('#dropdown').on('click', () => {
-   // 	 $('#dropdown1').hide();
-   // 	 $('#dropdown2').hide();
-   // });
+   $(window).on('scroll load' , function() {
+	if ($('.navbar').offset().top > 60) {
+		$(".fixed-top").addClass("navbar-custom-active");
+	}else {
+		$(".fixed-top").removeClass("navbar-custom-active");
+	}
+})
+
 });
--- js/scripts.js
+++ js/scripts.js
@@ -8,17 +8,7 @@
 (function($) {
     "use strict"; 
 	
-	/* Preloader */
-	$(window).on('load', function() {
-		var preloaderFadeOutTime = 3;
-		function hidePreloader() {
-			var preloader = $('.spinner-wrapper');
-			setTimeout(function() {
-				preloader.fadeOut(preloaderFadeOutTime);
-			}, 3);
-		}
-		hidePreloader();
-	});
+	
 
 	
 	/* Navbar Scripts */
@@ -31,16 +21,17 @@
 		}
     });
 
-	// jQuery for page scrolling feature - requires jQuery Easing plugin
-	$(function() {
-		$(document).on('click', 'a.page-scroll', function(event) {
-			var $anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $($anchor.attr('href')).offset().top
-			}, 600, 'easeInOutExpo');
-			event.preventDefault();
-		});
-	});
+   
+	// // jQuery for page scrolling feature - requires jQuery Easing plugin
+	// $(function() {
+	// 	$(document).on('click', 'a.page-scroll', function(event) {
+	// 		var $anchor = $(this);
+	// 		$('html, body').stop().animate({
+	// 			scrollTop: $($anchor.attr('href')).offset().top
+	// 		}, 600, 'easeInOutExpo');
+	// 		event.preventDefault();
+	// 	});
+	// });
 
     // closes the responsive menu on menu item click
     $(".navbar-nav li a").on("click", function(event) {
@@ -97,51 +88,51 @@
     });
     
 
-    /* Video Lightbox - Magnific Popup */
-    $('.popup-youtube, .popup-vimeo').magnificPopup({
-        disableOn: 700,
-        type: 'iframe',
-        mainClass: 'mfp-fade',
-        removalDelay: 160,
-        preloader: false,
-        fixedContentPos: false,
-        iframe: {
-            patterns: {
-                youtube: {
-                    index: 'youtube.com/', 
-                    id: function(url) {        
-                        var m = url.match(/[\\?\\&]v=([^\\?\\&]+)/);
-                        if ( !m || !m[1] ) return null;
-                        return m[1];
-                    },
-                    src: 'https://www.youtube.com/embed/%id%?autoplay=1'
-                },
-                vimeo: {
-                    index: 'vimeo.com/', 
-                    id: function(url) {        
-                        var m = url.match(/(https?:\/\/)?(www.)?(player.)?vimeo.com\/([a-z]*\/)*([0-9]{6,11})[?]?.*/);
-                        if ( !m || !m[5] ) return null;
-                        return m[5];
-                    },
-                    src: 'https://player.vimeo.com/video/%id%?autoplay=1'
-                }
-            }
-        }
-    });
+    // /* Video Lightbox - Magnific Popup */
+    // $('.popup-youtube, .popup-vimeo').magnificPopup({
+    //     disableOn: 700,
+    //     type: 'iframe',
+    //     mainClass: 'mfp-fade',
+    //     removalDelay: 160,
+    //     preloader: false,
+    //     fixedContentPos: false,
+    //     iframe: {
+    //         patterns: {
+    //             youtube: {
+    //                 index: 'youtube.com/', 
+    //                 id: function(url) {        
+    //                     var m = url.match(/[\\?\\&]v=([^\\?\\&]+)/);
+    //                     if ( !m || !m[1] ) return null;
+    //                     return m[1];
+    //                 },
+    //                 src: 'https://www.youtube.com/embed/%id%?autoplay=1'
+    //             },
+    //             vimeo: {
+    //                 index: 'vimeo.com/', 
+    //                 id: function(url) {        
+    //                     var m = url.match(/(https?:\/\/)?(www.)?(player.)?vimeo.com\/([a-z]*\/)*([0-9]{6,11})[?]?.*/);
+    //                     if ( !m || !m[5] ) return null;
+    //                     return m[5];
+    //                 },
+    //                 src: 'https://player.vimeo.com/video/%id%?autoplay=1'
+    //             }
+    //         }
+    //     }
+    // });
 
 
-    /* Lightbox - Magnific Popup */
-	$('.popup-with-move-anim').magnificPopup({
-		type: 'inline',
-		fixedContentPos: false, /* keep it false to avoid html tag shift with margin-right: 17px */
-		fixedBgPos: true,
-		overflowY: 'auto',
-		closeBtnInside: true,
-		preloader: false,
-		midClick: true,
-		removalDelay: 300,
-		mainClass: 'my-mfp-slide-bottom'
-	});
+    // /* Lightbox - Magnific Popup */
+	// $('.popup-with-move-anim').magnificPopup({
+	// 	type: 'inline',
+	// 	fixedContentPos: false, /* keep it false to avoid html tag shift with margin-right: 17px */
+	// 	fixedBgPos: true,
+	// 	overflowY: 'auto',
+	// 	closeBtnInside: true,
+	// 	preloader: false,
+	// 	midClick: true,
+	// 	removalDelay: 300,
+	// 	mainClass: 'my-mfp-slide-bottom'
+	// });
     
     
     /* Move Form Fields Label When User Types */
